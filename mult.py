7d705d6ed6fb4ad05500763a37766c98b05eec0e
Mark Nguyen <mnguyen4@apple.com>
2019-11-21 18:20:41 -0800

    Supported multiplying single digits

--- mult.py
+++ mult.py
@@ -5,15 +5,20 @@ Given two numeric strings, return their multiple
 import pytest
 
 def main(first, second):
-    return str(int(first) * int(second))
+    row_1 = []
+    for c in first:
+        value = int(second[0]) * int(c)
+        row_1.insert(0, str(value))
+    return ''.join(row_1)
 
 @pytest.mark.parametrize('a,b', [
     (1, 1),
-    # (1, 0),
-    # (1, 2),
-    # (2, 1),
-    # (2, 0),
-    # (2, 2),
+    (1, 0),
+    (1, 2),
+    (2, 1),
+    (2, 0),
+    (2, 2),
+    (20, 2),
     # (10, 10),
     # (123456789, 987654321),
     # (123456789987654321, 123456789987654321),
