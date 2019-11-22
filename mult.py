5f1ebc39464d2c28348ee0a5f5a08612314191b1
Mark Nguyen <mnguyen4@apple.com>
2019-11-21 18:25:48 -0800

    Support multiplying a single digit against multiple digits now

--- mult.py
+++ mult.py
@@ -5,10 +5,18 @@ Given two numeric strings, return their multiple
 import pytest
 
 def main(first, second):
+    # This stores the result of a single multiply
     row_1 = []
-    for c in first:
-        value = int(second[0]) * int(c)
-        row_1.insert(0, str(value))
+    carry = 0
+    for c in reversed(first):
+        value = int(second[0]) * int(c) + carry
+        row_1.insert(0, str(value %10))
+        carry = value / 10
+
+    # There might still be a carry from the final multiply, so add that
+    if carry:
+        row_1.insert(0, str(carry))
+
     return ''.join(row_1)
 
 @pytest.mark.parametrize('a,b', [
@@ -19,6 +27,9 @@ def main(first, second):
     (2, 0),
     (2, 2),
     (20, 2),
+    (13, 2),
+    (13, 4),
+    (12356, 9),
     # (10, 10),
     # (123456789, 987654321),
     # (123456789987654321, 123456789987654321),
